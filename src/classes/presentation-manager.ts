b4364a0334c94a2af5a625203b55c073387ebfb0
Rodrigo Alcorta <rodrigovictoralcorta@gmail.com>
2021-02-08 12:38:41 -0300

    Update presentation-manager.ts

--- src/classes/presentation-manager.ts
+++ src/classes/presentation-manager.ts
@@ -2,9 +2,8 @@ import * as chalk from 'chalk';
 
 export class PresentationManager {
     public static printPresentation() {
-        console.log(chalk.green('Hello '), chalk.green.bold('ESQUIVEL NESTOR GABRIEL!'));
         console.log('');
-        console.log(chalk.green('I\'m make this to make this more spicy :D, I hope you like this!!'));
+        console.log(chalk.green('By Rodrigo Alcorta.'));
         console.log(chalk.cyan('Enjoy!'));
         console.log();
     }
